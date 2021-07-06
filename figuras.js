417557da924d98eb9795969d00b9120a62e3a3d3
antac <angello_04_09@hotmail.com>
2021-07-06 18:09:06 -0500

    encapsulando codigo en funciones

--- figuras.js
+++ figuras.js
@@ -1,1 +1,36 @@
-console.log('hello world');
+//Código del cuadrado
+console.group('Cuadrados');
+const perimetroCuadrado = (lado)=> lado * 4;
+
+const areaCuadrado = (lado)=> lado ** 2;
+
+console.groupEnd();
+
+//Código del triángulo
+console.group('Triángulo')
+
+const perimetroTriangulo = (lado1,lado2,base)=> lado1 + lado2 + base;
+
+const areaTriangulo = (base,altura)=> (base * altura )/ 2;
+
+console.groupEnd();
+
+//Código del circulo
+console.group('Círculos');
+
+//Diametro
+const diametroCirculo = (radio)=> radio * 2 ;
+
+//PI
+/* const PI = 3.1415 */
+const PI = Math.PI;
+
+//Circunferencia
+const perimetroCirculo =(radio)=>{
+    const diametro = diametroCirculo(radio);
+    return diametro* PI
+};
+
+//Área
+const areaCirculo =(radio)=> (radio ** 2) * PI;
+console.groupEnd();
